1869edfc1fd5f105c75714e78100b30cf53174a6
Ogola Eddy <eddogola01@gmail.com>
2021-02-26 11:47:53 +0300

    comments on exported errors

--- errors.go
+++ errors.go
@@ -5,6 +5,8 @@ import (
 )
 
 var (
+	// ErrInvalidCharsInVideoID defines error when video ID has any of #%&</"
 	ErrInvalidCharsInVideoID = errors.New("invalid characters in video ID")
+	// ErrVideoIDMinLength defines error when video ID's length is less than 10
 	ErrVideoIDMinLength      = errors.New("Video ID less than min length")
 )
